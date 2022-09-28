ea18bb51ba8476a0158449fe33802168b9e9e674
Samy Kebli <jaivendutonchat@hotmail.com>
2022-09-28 14:47:54 +0200

    oui

--- app/controller/requests.php
+++ app/controller/requests.php
@@ -13,9 +13,8 @@ function GetAllUsers($db){
 		$nblignes = $stmt->rowCount();
 	    if($nblignes>=1){
 			while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-				echo"<option value='$DebSem' > Du $DebSem au $FinSem </option>";			
+				echo"";			
 			}
-			return $tab;
 				
 		}
 
