d82bb44a433543a3554119cfb7fd46cca17367d0
Julie Lecerre <you@example.com>
2022-09-29 09:27:11 +0200

    inscription and login files

--- app/view/login.php
+++ app/view/login.php
@@ -13,20 +13,19 @@
 <!-- partial:index.partial.html -->
 <div class="login-form">
   <form method="POST">
-	
-    <h1>Vous connecter</h1>
-    <div class="content">
-      <div class="input-field">
-        <input type="text" placeholder="Email" name="form_email" autocomplete="nope" required>
-      </div>
-      <div class="input-field">
-        <input type="password" placeholder="Mot de passe" name="form_motdepasse" autocomplete="new-password" required>
-      </div>
-    </div>
-    <div class="action">
-	  <a href="adduser.php"><button type="button" class="btn btn-light btn-lg">Je m'inscris</button></a>
-		<button type="submit" name="valider" class="btn btn-light btn-lg">Je me connecte</button>
-    </div>
+	<div class="container">
+		<div class="row justify-content-center text-center p-5">
+			<div class="card" style="width: 48rem;">
+					<div class="card-header">Se connecter</div>
+						<input class="form-control" type="text" name="form_email" placeholder="Email" required>
+						<input class="form-control" type="password" name="form_motdepasses" placeholder="Mot de passe" required>
+			</div>
+		</div>
+	</div>
+	<div class="row justify-content-center">
+	<a href="inscription.php">	<button type="submit" name="valider" class="btn btn-light btn-lg" required>Je m'inscris</button></a>
+	  <button type="button" class="btn btn-light btn-lg" required>Je me connecte</button>
+	</div>
   </form>			
   <p> <?php if(isset($message) !== "" || isset($message) !== null){echo $message;}else{} ?></p>
 </div>
