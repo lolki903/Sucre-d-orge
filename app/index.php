f0728c69d05ad2c0cc9a637ff7fb30b4e58a1ff1
Samy Kebli <jaivendutonchat@hotmail.com>
2022-09-30 11:44:28 +0200

    encore plus de bugfix

--- app/index.php
+++ app/index.php
@@ -19,6 +19,18 @@ if($_SESSION['type'] === 1){
 
 ?>
 
+<!doctype html>
+<html>
+<head>
+    <meta>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css">
+  <link rel="stylesheet" type="text/css" href="assets/css/chat.css">
+  <script>
+        var Nom = <?php echo json_encode($_SESSION['firstname']); ?>;
+    </script>
+  
+</head>
 <body class="bg-danger">
     <div id="app" class="container-fluid">
         <!--navbar-->
